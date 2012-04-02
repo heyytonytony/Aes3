af3bf3abfe1fddd0d59b9c937fc56ada367a4bcf
colin725 <colin725@gmail.com>
2012-04-02 16:43:55 -0400

    full questions

--- Assets/Aes3/Scripts/CarController.cs
+++ Assets/Aes3/Scripts/CarController.cs
@@ -8,7 +8,7 @@ public class CarController : MonoBehaviour {
 
 	//state
 	public int state = 0;
-	private bool[] flags = new bool[10];
+	private bool[] flags = new bool[50];
 	public Texture2D flag;
 	
 	// Add all wheels of the car here, so brake and steering forces can be applied to them.
@@ -235,28 +235,166 @@ public class CarController : MonoBehaviour {
 		if( centerOfMass.position.x > 1130 && centerOfMass.position.x < 1170 
 			&& centerOfMass.position.z > 525 && centerOfMass.position.z < 570)
 		{
-			if(flags[1] == false){
-				state = 1;
-				Time.timeScale = 0;
-				rigidbody.SetMaxAngularVelocity(0);
+			if(currentLap == 0){
+				if(flags[1] == false){
+					state = 1;
+					Time.timeScale = 0;
+					rigidbody.SetMaxAngularVelocity(0);
+				}
+				else if(flags[2] == false){
+					state = 2;
+				}
+				else if(flags[3] == false){
+					state = 3;
+				}
+				else if(flags[4] == false){
+					state = 4;
+				}
+				else if(flags[5] == false){
+					state = 5;
+				}
+				else {
+					state = 0;
+					Time.timeScale = 1;
+					rigidbody.SetMaxAngularVelocity(1);
+				}
 			}
-			else if(flags[2] == false){
-				state = 2;
+			else if( currentLap == 1){
+				if(flags[6] == false){
+					state = 6;
+					Time.timeScale = 0;
+					rigidbody.SetMaxAngularVelocity(0);
+				}
+				else if(flags[7] == false){
+					state = 7;
+				}
+				else {
+					state = 0;
+					Time.timeScale = 1;
+					rigidbody.SetMaxAngularVelocity(1);
+				}
 			}
-			else if(flags[3] == false){
-				state = 3;
+			else if( currentLap == 2){
+				if(flags[8] == false){
+					state = 8;
+					Time.timeScale = 0;
+					rigidbody.SetMaxAngularVelocity(0);
+				}
+				else if(flags[9] == false){
+					state = 9;
+				}
+				else {
+					state = 0;
+					Time.timeScale = 1;
+					rigidbody.SetMaxAngularVelocity(1);
+				}
 			}
-			else if(flags[4] == false){
-				state = 4;
+			else if( currentLap == 3){
+				if(flags[10] == false){
+					state = 10;
+					Time.timeScale = 0;
+					rigidbody.SetMaxAngularVelocity(0);
+				}
+				else if(flags[11] == false){
+					state = 11;
+				}
+				else {
+					state = 0;
+					Time.timeScale = 1;
+					rigidbody.SetMaxAngularVelocity(1);
+				}
 			}
-			else if(flags[5] == false){
-				state = 5;
+			else if( currentLap == 4){
+				if(flags[12] == false){
+					state = 12;
+					Time.timeScale = 0;
+					rigidbody.SetMaxAngularVelocity(0);
+				}
+				else if(flags[13] == false){
+					state = 13;
+				}
+				else {
+					state = 0;
+					Time.timeScale = 1;
+					rigidbody.SetMaxAngularVelocity(1);
+				}
 			}
-			else {
-				state = 0;
-				Time.timeScale = 1;
-				rigidbody.SetMaxAngularVelocity(1);
+			else if( currentLap == 5){
+				if(flags[14] == false){
+					state = 14;
+					Time.timeScale = 0;
+					rigidbody.SetMaxAngularVelocity(0);
+				}
+				else if(flags[15] == false){
+					state = 15;
+				}
+				else {
+					state = 0;
+					Time.timeScale = 1;
+					rigidbody.SetMaxAngularVelocity(1);
+				}
 			}
+			else if( currentLap == 6){
+				if(flags[16] == false){
+					state = 16;
+					Time.timeScale = 0;
+					rigidbody.SetMaxAngularVelocity(0);
+				}
+				else if(flags[17] == false){
+					state = 17;
+				}
+				else {
+					state = 0;
+					Time.timeScale = 1;
+					rigidbody.SetMaxAngularVelocity(1);
+				}
+			}
+			else if( currentLap == 7){
+				if(flags[18] == false){
+					state = 18;
+					Time.timeScale = 0;
+					rigidbody.SetMaxAngularVelocity(0);
+				}
+				else if(flags[19] == false){
+					state = 19;
+				}
+				else {
+					state = 0;
+					Time.timeScale = 1;
+					rigidbody.SetMaxAngularVelocity(1);
+				}
+			}
+			else if( currentLap == 8){
+				if(flags[20] == false){
+					state = 20;
+					Time.timeScale = 0;
+					rigidbody.SetMaxAngularVelocity(0);
+				}
+				else if(flags[21] == false){
+					state = 21;
+				}
+				else {
+					state = 0;
+					Time.timeScale = 1;
+					rigidbody.SetMaxAngularVelocity(1);
+				}
+			}
+			else if( currentLap == 9){
+				if(flags[22] == false){
+					state = 22;
+					Time.timeScale = 0;
+					rigidbody.SetMaxAngularVelocity(0);
+				}
+				else if(flags[23] == false){
+					state = 23;
+				}
+				else {
+					state = 0;
+					Time.timeScale = 1;
+					rigidbody.SetMaxAngularVelocity(1);
+				}
+			}
+			
 		}
 		else if(currentLap == 10)
 		{
@@ -272,6 +410,7 @@ public class CarController : MonoBehaviour {
 	
 	void OnGUI()
 	{
+		//LAP 1
 		if(state == 1){
 			// Make a box
 			GUI.DrawTexture(new Rect(Screen.width/2-200, Screen.height/2-300, 400, 400), flag);
@@ -315,6 +454,203 @@ public class CarController : MonoBehaviour {
 				flags[5] = true;
 			}
 		}
+		
+		
+		//LAP 2
+		if(state == 6){
+			// Make a box
+			GUI.DrawTexture(new Rect(Screen.width/2-200, Screen.height/2-300, 400, 400), flag);			
+			GUI.Box(new Rect(Screen.width/2-150,Screen.height/2-150,300,300), "\n\nCheckpoint 2\n\n For the second checkpoint \nwe will we will give you a choice\n of stock seats\n or lighter racing seats.");
+			if(GUI.Button(new Rect(Screen.width/2-50,Screen.height/2+50,100,20), "Continue")) {
+				flags[6] = true;
+			}
+		}
+		if(state == 7){
+			// Make a box
+			GUI.DrawTexture(new Rect(Screen.width/2-200, Screen.height/2-300, 400, 400), flag);			
+			GUI.Box(new Rect(Screen.width/2-150,Screen.height/2-150,300,300), "\n\nCheckpoint 2\n\n F=M*A \nor Acceleration = force / mass\n\n your force will stay constant for now\n your choice is between lighter mass \n or heavier mass.");
+			if(GUI.Button(new Rect(Screen.width/2-125,Screen.height/2+40,250,20), "Stock seats  (M = 170 KG)")) {
+				flags[7] = true;
+			}
+			if(GUI.Button(new Rect(Screen.width/2-125,Screen.height/2+70,250,20), "Racing seats  (M = 60 KG)")) {
+				flags[7] = true;
+			}
+		}
+		
+		
+		//LAP 3
+		if(state == 8){
+			// Make a box
+			GUI.DrawTexture(new Rect(Screen.width/2-200, Screen.height/2-300, 400, 400), flag);			
+			GUI.Box(new Rect(Screen.width/2-150,Screen.height/2-150,300,300), "\n\nCheckpoint 3\n\n For the third checkpoint \nwe will we will give you a choice\n of stock wheels\n or wheels with better grip.");
+			if(GUI.Button(new Rect(Screen.width/2-50,Screen.height/2+50,100,20), "Continue")) {
+				flags[8] = true;
+			}
+		}
+		if(state == 9){
+			// Make a box
+			GUI.DrawTexture(new Rect(Screen.width/2-200, Screen.height/2-300, 400, 400), flag);			
+			GUI.Box(new Rect(Screen.width/2-150,Screen.height/2-150,300,300), "\n\nCheckpoint 3\n\n F=M*A \nor Acceleration = force / mass\n\n your mass will stay constant for now\n your choice is between wheels. \n Wheels with more grip increases your force.");
+			if(GUI.Button(new Rect(Screen.width/2-125,Screen.height/2+40,250,20), "Stock wheels  (F = 400 Newtons)")) {
+				flags[9] = true;
+			}
+			if(GUI.Button(new Rect(Screen.width/2-125,Screen.height/2+70,250,20), "Racing seats  (F = 450 Newtons)")) {
+				flags[9] = true;
+			}
+		}
+		
+		
+		//LAP 4
+		if(state == 10){
+			// Make a box
+			GUI.DrawTexture(new Rect(Screen.width/2-200, Screen.height/2-300, 400, 400), flag);			
+			GUI.Box(new Rect(Screen.width/2-150,Screen.height/2-150,300,300), "\n\nCheckpoint 4\n\n For the first checkpoint \nwe will we will give you a choice\n of a stock hood\n or lighter racing hood.");
+			if(GUI.Button(new Rect(Screen.width/2-50,Screen.height/2+50,100,20), "Continue")) {
+				flags[10] = true;
+			}
+		}
+		if(state == 11){
+			// Make a box
+			GUI.DrawTexture(new Rect(Screen.width/2-200, Screen.height/2-300, 400, 400), flag);			
+			GUI.Box(new Rect(Screen.width/2-150,Screen.height/2-150,300,300), "\n\nCheckpoint 4\n\n F=M*A \nor Acceleration = force / mass\n\n your force will stay constant for now\n your choice is between lighter mass \n or heavier mass.");
+			if(GUI.Button(new Rect(Screen.width/2-125,Screen.height/2+40,250,20), "Stock hood  (M = 90 KG)")) {
+				flags[11] = true;
+			}
+			if(GUI.Button(new Rect(Screen.width/2-125,Screen.height/2+70,250,20), "Racing hood  (M = 55 KG)")) {
+				flags[11] = true;
+			}
+		}
+		
+		//LAP 5
+		if(state == 12){
+			// Make a box
+			GUI.DrawTexture(new Rect(Screen.width/2-200, Screen.height/2-300, 400, 400), flag);			
+			GUI.Box(new Rect(Screen.width/2-150,Screen.height/2-150,300,300), "\n\nCheckpoint 5\n\n For the fifth checkpoint \nwe will we will give you a choice\n of stock engine\n or an engine with more force.");
+			if(GUI.Button(new Rect(Screen.width/2-50,Screen.height/2+50,100,20), "Continue")) {
+				flags[12] = true;
+			}
+		}
+		if(state == 13){
+			// Make a box
+			GUI.DrawTexture(new Rect(Screen.width/2-200, Screen.height/2-300, 400, 400), flag);			
+			GUI.Box(new Rect(Screen.width/2-150,Screen.height/2-150,300,300), "\n\nCheckpoint 5\n\n F=M*A \nor Acceleration = force / mass\n\n your mass will stay constant for now\n your choice is between engines. \n");
+			if(GUI.Button(new Rect(Screen.width/2-125,Screen.height/2+40,250,20), "Stock engine  (F = 600 Newtons)")) {
+				flags[13] = true;
+			}
+			if(GUI.Button(new Rect(Screen.width/2-125,Screen.height/2+70,250,20), "new engine  (F = 800 Newtons)")) {
+				flags[13] = true;
+			}
+		}
+		
+		
+		//LAP 6
+		if(state == 14){
+			// Make a box
+			GUI.DrawTexture(new Rect(Screen.width/2-200, Screen.height/2-300, 400, 400), flag);			
+			GUI.Box(new Rect(Screen.width/2-150,Screen.height/2-150,300,300), "\n\nCheckpoint 6\n\n For the sixth checkpoint \nwe will we will give you a choice\n of a stock doors\n or lighter racing doors.");
+			if(GUI.Button(new Rect(Screen.width/2-50,Screen.height/2+50,100,20), "Continue")) {
+				flags[14] = true;
+			}
+		}
+		if(state == 15){
+			// Make a box
+			GUI.DrawTexture(new Rect(Screen.width/2-200, Screen.height/2-300, 400, 400), flag);			
+			GUI.Box(new Rect(Screen.width/2-150,Screen.height/2-150,300,300), "\n\nCheckpoint 6\n\n F=M*A \nor Acceleration = force / mass\n\n your force will stay constant for now\n your choice is between lighter mass \n or heavier mass.");
+			if(GUI.Button(new Rect(Screen.width/2-125,Screen.height/2+40,250,20), "Stock doors  (M = 85 KG)")) {
+				flags[15] = true;
+			}
+			if(GUI.Button(new Rect(Screen.width/2-125,Screen.height/2+70,250,20), "Racing doors  (M = 70 KG)")) {
+				flags[15] = true;
+			}
+		}
+		
+				//LAP 7
+		if(state == 16){
+			// Make a box
+			GUI.DrawTexture(new Rect(Screen.width/2-200, Screen.height/2-300, 400, 400), flag);			
+			GUI.Box(new Rect(Screen.width/2-150,Screen.height/2-150,300,300), "\n\nCheckpoint 7\n\n For the seventh checkpoint \nwe will we will give you a choice\n of adding a turbo\n A turbo will add more force.");
+			if(GUI.Button(new Rect(Screen.width/2-50,Screen.height/2+50,100,20), "Continue")) {
+				flags[16] = true;
+			}
+		}
+		if(state == 17){
+			// Make a box
+			GUI.DrawTexture(new Rect(Screen.width/2-200, Screen.height/2-300, 400, 400), flag);			
+			GUI.Box(new Rect(Screen.width/2-150,Screen.height/2-150,300,300), "\n\nCheckpoint 7\n\n F=M*A \nor Acceleration = force / mass\n\n your mass will stay constant for now\n your choice is between turbos. \n");
+			if(GUI.Button(new Rect(Screen.width/2-125,Screen.height/2+40,250,20), "No turbo  (F = 600 Newtons)")) {
+				flags[17] = true;
+			}
+			if(GUI.Button(new Rect(Screen.width/2-125,Screen.height/2+70,250,20), "Add a turbo  (F = 800 Newtons)")) {
+				flags[17] = true;
+			}
+		}
+		
+		
+		//LAP 8
+		if(state == 18){
+			// Make a box
+			GUI.DrawTexture(new Rect(Screen.width/2-200, Screen.height/2-300, 400, 400), flag);			
+			GUI.Box(new Rect(Screen.width/2-150,Screen.height/2-150,300,300), "\n\nCheckpoint 8\n\n For the eigth checkpoint \nwe will we will give you a choice\n of what to put in\n your trunk.");
+			if(GUI.Button(new Rect(Screen.width/2-50,Screen.height/2+50,100,20), "Continue")) {
+				flags[18] = true;
+			}
+		}
+		if(state == 19){
+			// Make a box
+			GUI.DrawTexture(new Rect(Screen.width/2-200, Screen.height/2-300, 400, 400), flag);			
+			GUI.Box(new Rect(Screen.width/2-150,Screen.height/2-150,300,300), "\n\nCheckpoint 8\n\n F=M*A \nor Acceleration = force / mass\n\n your force will stay constant for now\n your choice is between nothing \n or a heavier mass in your trunk.");
+			if(GUI.Button(new Rect(Screen.width/2-125,Screen.height/2+40,250,20), "Nothing  (M = 0 KG)")) {
+				flags[19] = true;
+			}
+			if(GUI.Button(new Rect(Screen.width/2-125,Screen.height/2+70,250,20), "Scuba Gear  (M = 105 KG)")) {
+				flags[19] = true;
+			}
+		}
+		
+				//LAP 9
+		if(state == 20){
+			// Make a box
+			GUI.DrawTexture(new Rect(Screen.width/2-200, Screen.height/2-300, 400, 400), flag);			
+			GUI.Box(new Rect(Screen.width/2-150,Screen.height/2-150,300,300), "\n\nCheckpoint 9\n\n For the ninth checkpoint \nwe will we will give you a choice\n of V6 engine\n or an engine with more force.");
+			if(GUI.Button(new Rect(Screen.width/2-50,Screen.height/2+50,100,20), "Continue")) {
+				flags[20] = true;
+			}
+		}
+		if(state == 21){
+			// Make a box
+			GUI.DrawTexture(new Rect(Screen.width/2-200, Screen.height/2-300, 400, 400), flag);			
+			GUI.Box(new Rect(Screen.width/2-150,Screen.height/2-150,300,300), "\n\nCheckpoint 9\n\n F=M*A \nor Acceleration = force / mass\n\n your mass will stay constant for now\n your choice is between engines. \n");
+			if(GUI.Button(new Rect(Screen.width/2-125,Screen.height/2+40,250,20), "V6 engine  (F = 800 Newtons)")) {
+				flags[21] = true;
+			}
+			if(GUI.Button(new Rect(Screen.width/2-125,Screen.height/2+70,250,20), "V8 engine  (F = 900 Newtons)")) {
+				flags[21] = true;
+			}
+		}
+		
+		
+		//LAP 10
+		if(state == 22){
+			// Make a box
+			GUI.DrawTexture(new Rect(Screen.width/2-200, Screen.height/2-300, 400, 400), flag);			
+			GUI.Box(new Rect(Screen.width/2-150,Screen.height/2-150,300,300), "\n\nCheckpoint 10\n\n For the tenth checkpoint \nwe will we will give you a choice\n of no passengers\n or bring your little cousin.");
+			if(GUI.Button(new Rect(Screen.width/2-50,Screen.height/2+50,100,20), "Continue")) {
+				flags[22] = true;
+			}
+		}
+		if(state == 23){
+			// Make a box
+			GUI.DrawTexture(new Rect(Screen.width/2-200, Screen.height/2-300, 400, 400), flag);			
+			GUI.Box(new Rect(Screen.width/2-150,Screen.height/2-150,300,300), "\n\nCheckpoint 10\n\n F=M*A \nor Acceleration = force / mass\n\n your force will stay constant for now\n your choice is between lighter mass \n or heavier mass.");
+			if(GUI.Button(new Rect(Screen.width/2-125,Screen.height/2+40,250,20), "No passenger  (M = 0 KG)")) {
+				flags[23] = true;
+			}
+			if(GUI.Button(new Rect(Screen.width/2-125,Screen.height/2+70,250,20), "Little Richie  (M = 97 KG)")) {
+				flags[23] = true;
+			}
+		}
+		
+		
 		GUI.Label(new Rect(Screen.width-250,60,200,50),"Center:" + centerOfMass.position +"\n" + "State: " + state, "box");
 		//GUI.Label(new Rect(Screen.width-100,60,100,200),"km/h: " + rigidbody.velocity.magnitude * 3.6f, "box");
 		//tractionControl = GUI.Toggle(new Rect(0,80,300,20), tractionControl, "Traction Control (bypassed by shift key)");
